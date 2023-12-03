e94bcc563820c46eec446d2344be7393e01dc255
Leonardo Rodrigues <48986533+leo0grau@users.noreply.github.com>
2023-12-03 19:34:05 -0300

    lista de projetos

--- src/pages/projetos/index.tsx
+++ src/pages/projetos/index.tsx
@@ -1,11 +1,44 @@
-import {View, Text, TouchableOpacity} from 'react-native';
-import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, ScrollView} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import styles from './styles';
-
+import {useIsFocused} from '@react-navigation/native';
+import getRealm from '../../database/realm';
+import IonIcons from 'react-native-vector-icons/Ionicons';
+import ItemComponent from './item';
 export default function ProjetosPage() {
   const [completo, setcompleto] = useState(false);
+  const [listaProjetos, setListaProjetos] = useState<any[]>([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    const getData = async () => {
+      const realm = await getRealm();
+      const projetos: any = realm.objects('Projetos');
+      setListaProjetos(projetos);
+    };
+    try {
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [completo, isFocused]);
+
   return (
-    <View>
+    <ScrollView style={{flex: 1}}>
+      <View
+        style={{
+          marginHorizontal: 19,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: 41,
+        }}>
+        <Text
+          style={{fontFamily: 'Poppins-Bold', color: 'black', fontSize: 18}}>
+          Lista de Projetos
+        </Text>
+        <View />
+      </View>
       <View style={styles.linha}>
         <TouchableOpacity
           style={[!completo ? styles.selected : styles.normal]}
@@ -22,8 +55,12 @@ export default function ProjetosPage() {
           </Text>
         </TouchableOpacity>
       </View>
-
-      <Text>Projetos</Text>
-    </View>
+      <View style={{marginTop: 30}}>
+        {listaProjetos.map((v, i) => {
+          return <ItemComponent key={i} item={v} />;
+        })}
+      </View>
+      <Text></Text>
+    </ScrollView>
   );
 }
--- src/pages/projetos/item.tsx
+++ src/pages/projetos/item.tsx
@@ -1,10 +1,21 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
-export default function ItemComponent() {
+export default function ItemComponent({item}: any) {
   return (
-    <View>
-      <Text>item</Text>
+    <View
+      style={{
+        marginHorizontal: 53,
+        backgroundColor: 'white',
+        elevation: 10,
+        borderRadius: 16,
+        padding: 24,
+        marginBottom: 13,
+      }}>
+      <Text
+        style={{color: '#1C1243', fontSize: 18, fontFamily: 'Poppins-Light'}}>
+        {item.nome_projeto}
+      </Text>
     </View>
   );
 }
